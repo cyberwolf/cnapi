23053c54c326bdcd5caa3320c8438a646c586421
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2010-10-21 20:17:15 +0200

    Using "request" instead of "params"

--- cnapi.api.php
+++ cnapi.api.php
@@ -2,32 +2,32 @@
 // $Id$
 
 /**
- * Alter the params from a request done using cnapi_get. Mostly used to add some defaults.
+ * Alter the request from a request done using cnapi_get. Mostly used to add some defaults.
  *
- * @param $params
- *   The params for the request.
+ * @param $request
+ *   The request for the request.
  */
-function hook_cnapi_params_defaults_alter(&$params) {
-  $params['pagelength'] = 10;
+function hook_cnapi_request_defaults_alter(&$request) {
+  $request['pagelength'] = 10;
 }
 
 /**
- * Before a request is sent to the API it is validated. By default this only checks the format of the $params like:
+ * Before a request is sent to the API it is validated. By default this only checks the format of the request like:
  * - Is a key provided?
  * - Is a valid action and type provided?
  * - Are valid request filters provided in case of listings and reports?
  *
  * Using this hook you can add extra validation like for example limiting the allowed filters, only allow certain keys, ...
  *
- * @param $params
- *   The params for the request.
+ * @param $request
+ *   The request for the request.
  *
  * @return
- *   A boolean indicated if the $params are is valid or not.
+ *   A boolean indicated if the $request are is valid or not.
  */
-function hook_cnapi_params_validate($params) {
+function hook_cnapi_request_validate($request) {
   // maximum pagelength is 20
-  if (isset($params['query']['pagelength']) && $params['query']['pagelength'] > 20) {
+  if (isset($request['query']['pagelength']) && $request['query']['pagelength'] > 20) {
     return FALSE;
   }
 }
