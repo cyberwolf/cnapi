99a0aa9930f365899d4cb6301002ed4fc8050beb
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-03 19:25:48 +0200

    code cleanup

--- cnapi_browse/cnapi-browse-filter-option.tpl.php
+++ cnapi_browse/cnapi-browse-filter-option.tpl.php
@@ -1,11 +1,11 @@
 <?php if ($active) : ?>
-	<?php print $name ?>
+  <?php print $name ?>
 <?php else : ?>
-	<?php print $link ?>
+  <?php print $link ?>
 <?php endif; ?>
 
 <?php if (isset($total)) : ?>(<?php print $total ?>)<?php endif; ?>
 
 <?php if ($active) : ?>
-	<?php print $link_remove ?>
+  <?php print $link_remove ?>
 <?php endif; ?>
--- cnapi_browse/cnapi-browse-navigation.tpl.php
+++ cnapi_browse/cnapi-browse-navigation.tpl.php
@@ -1,10 +1,10 @@
 <ul>
 <?php foreach ($items as $item) : ?>
-	<li>
-		<?php print theme('cnapi_browse_navigation_item', array('item' => $item)) ?>
-		<?php if (!empty($item['children'])) : ?>
-			<?php print theme('cnapi_browse_navigation', array('items' => $item['children'])); ?>
-		<?php endif; ?>
-	</li>
+  <li>
+    <?php print theme('cnapi_browse_navigation_item', array('item' => $item)) ?>
+    <?php if (!empty($item['children'])) : ?>
+      <?php print theme('cnapi_browse_navigation', array('items' => $item['children'])); ?>
+    <?php endif; ?>
+  </li>
 <?php endforeach; ?>
 </ul>
