f49a8d608cbb721fb764ea0bb4dab0f08f6919ae
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-04 09:49:56 +0200

    more generic datepicker and doc

--- cnapi_browse/js/cnapi_browse.js
+++ cnapi_browse/js/cnapi_browse.js
@@ -11,19 +11,34 @@
 
   Drupal.behaviors.cnapiBrowseDatePicker = {
     attach: function (context, settings) {
-      $('.form-item:has(.has-datepicker)').append('<div id="edit-when-datepicker" class="date-picker"></div>');
+      // hide the date field
+      $('.has-datepicker').each(function () {
+        var sel = '.form-item-' + $(this).attr('id') + '-date';
+        sel = sel.replace('form-item-edit', 'form-item');
+        $(sel).hide();
+      });
+      
+      // add the datepicker
+      $('.form-item:has(.has-datepicker)').each(function () {
+        $(this).append('<div rel="' + $('select', this).attr('id') + '" class="date-picker"></div>');
+      });
 
+      // add extra option to select field to display datepicker
       $('.has-datepicker').append($("<option></option>").val("_datepicker").text(Drupal.t('Pick a date')));
 
-	    $('.has-datepicker')
-        .bind('change', function () {
-          if ($(this).val() == '_datepicker') {
-            var id = '#'+ $(this).attr('id') +'-datepicker';
-            $(id).dpDisplay();
-          }
-          $(this).val('_none');
-          return false;
-        });
+      // bind onchange event to show the datepicker when the "pick a date" option has been chosen
+	    $('.has-datepicker').bind('change', function () {
+        if ($(this).val() == '_datepicker') {
+          $('.date-picker[rel=' + $(this).attr('id') + ']').dpDisplay();
+        }
+        
+        // we need to reset the selected option so we won't submit invalid options to Drupal
+        $(this).val('_none');
+        
+        return false;
+      });
+
+      // create the datepicker and bind the "dateSelected" event
 	    $('.date-picker')
 	      .datePicker({
 	        createButton: false,
@@ -32,6 +47,7 @@
           selectMultiple: true
         })
 	    	.bind('dateSelected', function (e, selectedDate, $td, state) {
+	    	  // when picking a date, we add or remove it from the date input field
           var selectedDates = $(this).dpGetSelected();
           var result = new Array();
 
@@ -39,7 +55,7 @@
             result.push(selectedDates[i].asString('yyyy-mm-dd'));
           }
 
-          $('input[name=when_date]').val(result.join(';'));
+          $('#' + $(this).attr('rel') + '-date').val(result.join(';'));
         });
       }
   };
