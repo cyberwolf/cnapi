b2fa8144843fba43f729525abc092a522aaa97b7
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-05 09:11:26 +0200

    adding initial version of cnapi-ui-event-summary

---
+++ cnapi_ui/cnapi-ui-event-summary.tpl.php
@@ -0,0 +1,9 @@
+<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <h2<?php print $title_attributes; ?>>
+    <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
+  </h2>
+  <?php print render($title_suffix); ?>
+  
+</div>
