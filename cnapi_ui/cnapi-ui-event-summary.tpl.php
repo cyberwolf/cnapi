9d676ecaea8c33a1b7331b974f3adb9ecc2a07b1
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-05 10:24:30 +0200

    finishing cnapi_ui_event_summary

--- cnapi_ui/cnapi-ui-event-summary.tpl.php
+++ cnapi_ui/cnapi-ui-event-summary.tpl.php
@@ -6,4 +6,35 @@
   </h2>
   <?php print render($title_suffix); ?>
   
+	<?php if ($thumbnail) : ?>
+		<a href="<?php print $object_url ?>"><img src="<?php print $thumbnail ?>" alt="<?php print $title ?>"/></a>
+	<?php endif; ?>
+	
+	<?php if ($shortdescription) : ?>
+	  <div class="description">
+	    <?php print $shortdescription ?>
+	  </div>
+	<?php endif; ?>
+	
+	<?php if (/*$performers || $where || $when*/TRUE) : ?>
+	  <dl>
+	    
+	    <?php if ($performers) : ?>
+	      <dt>Wie</dt>
+	      <dd><?php print $performers ?></dd>
+	    <?php endif; ?>
+	    
+	    <?php if ($where) : ?>
+	      <dt>Waar</dt>
+	      <dd><?php print $where ?></dd>
+	    <?php endif; ?>
+	    
+	    <?php if ($when) : ?>
+	      <dt>Wanneer</dt>
+	      <dd><?php print $when ?></dd>
+	    <?php endif; ?>
+	    
+	  </dl>
+	<?php endif; ?>
+  
 </div>
