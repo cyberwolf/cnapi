31eaabebd5cbb84365d66c8af55340e7b3a912f4
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-05 10:48:51 +0200

    cnapi_ui_actor_summary and refactory object summary preprocessing

---
+++ cnapi_ui/cnapi-ui-actor-summary.tpl.php
@@ -0,0 +1,19 @@
+<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <h2<?php print $title_attributes; ?>>
+    <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
+  </h2>
+  <?php print render($title_suffix); ?>
+  
+	<?php if ($thumbnail) : ?>
+		<a href="<?php print $object_url ?>"><img src="<?php print $thumbnail ?>" alt="<?php print $title ?>"/></a>
+	<?php endif; ?>
+	
+	<?php if ($shortdescription) : ?>
+	  <div class="description">
+	    <?php print $shortdescription ?>
+	  </div>
+	<?php endif; ?>
+  
+</div>
