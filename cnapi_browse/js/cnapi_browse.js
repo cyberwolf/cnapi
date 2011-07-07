50c18b92357225b3e459a0a4d13544cb141efc3a
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-07 11:40:27 +0200

    code cleanup

--- cnapi_browse/js/cnapi_browse.js
+++ cnapi_browse/js/cnapi_browse.js
@@ -1,84 +1,86 @@
 (function ($) {
-  $.dpText = {
-	  TEXT_PREV_YEAR		:	Drupal.t('Previous year'),
-	  TEXT_PREV_MONTH		:	Drupal.t('Previous month'),
-	  TEXT_NEXT_YEAR		:	Drupal.t('Next year'),
-	  TEXT_NEXT_MONTH		:	Drupal.t('Next month'),
-	  TEXT_CLOSE			  :	Drupal.t('Close'),
-	  TEXT_CHOOSE_DATE	:	Drupal.t('Choose date'),
-	  HEADER_FORMAT		  :	'mmmm yyyy'
-  };
 
-  Drupal.behaviors.cnapiBrowseAutoSubmitForm = {
-    attach: function (context, settings) {
-      $('.autosubmit').change(function() {
-        if ($(this).val() != '_none') {
-          $(this).parents('form').submit();
-          $(':input', $(this).parents('form')).attr('disabled', true);
-        }
-      });
-    }
-  };
+$.dpText = {
+  TEXT_PREV_YEAR		:	Drupal.t('Previous year'),
+  TEXT_PREV_MONTH		:	Drupal.t('Previous month'),
+  TEXT_NEXT_YEAR		:	Drupal.t('Next year'),
+  TEXT_NEXT_MONTH		:	Drupal.t('Next month'),
+  TEXT_CLOSE			  :	Drupal.t('Close'),
+  TEXT_CHOOSE_DATE	:	Drupal.t('Choose date'),
+  HEADER_FORMAT		  :	'mmmm yyyy'
+};
 
-  Drupal.behaviors.cnapiBrowseDatePicker = {
-    attach: function (context, settings) {
+Drupal.behaviors.cnapiBrowseAutoSubmitForm = {
+  attach: function (context, settings) {
+    $('.autosubmit').change(function() {
+      if ($(this).val() != '_none') {
+        $(this).parents('form').submit();
+        $(':input', $(this).parents('form')).attr('disabled', true);
+      }
+    });
+  }
+};
 
-      // hide the date field
-      $('.form-item:has(.date-for-datepicker)').hide();
+Drupal.behaviors.cnapiBrowseDatePicker = {
+  attach: function (context, settings) {
 
-      // hide the date field for fields and add the datepicker
-      $('.form-item:has(.date-for-datepicker)').after('<div class="datepicker"></div>');
+    // hide the date field
+    $('.form-item:has(.date-for-datepicker)').hide();
 
-      // add extra option to select field to display datepicker
-      $('.has-datepicker').append($("<option></option>").val("_datepicker").text(Drupal.t('Pick a date')));
+    // hide the date field for fields and add the datepicker
+    $('.form-item:has(.date-for-datepicker)').after('<div class="datepicker"></div>');
 
-      // bind onchange event to show the datepicker when the "pick a date" option has been chosen
-	    $('.has-datepicker').bind('change', function () {
-        if ($(this).val() == '_datepicker') {
-          $('.datepicker', $(this).parents('form')).dpDisplay();
+    // add extra option to select field to display datepicker
+    $('.has-datepicker').append($("<option></option>").val("_datepicker").text(Drupal.t('Pick a date')));
 
-          // we need to reset the selected option so we won't submit invalid options to Drupal
-          $(this).val('_none');
-        }
+    // bind onchange event to show the datepicker when the "pick a date" option has been chosen
+    $('.has-datepicker').bind('change', function () {
+      if ($(this).val() == '_datepicker') {
+        $('.datepicker', $(this).parents('form')).dpDisplay();
 
-        return false;
-      });
+        // we need to reset the selected option so we won't submit invalid options to Drupal
+        $(this).val('_none');
+      }
 
-      // create the datepicker and bind the "dateSelected" event
-	    $('.datepicker')
-	      .each(function () {
-	        var inline = $(this).parents('form').has('.has-datepicker').length == 0;
+      return false;
+    });
 
-	        $(this).datePicker({
-	          createButton: false,
-            displayClose: true,
-            inline: inline,
-            closeOnSelect: false,
-            selectMultiple: true
-          });
-	      })
-	    	.bind('dateSelected', function (e, selectedDate, $td, state) {
-	    	  // when picking a date, we add or remove it from the date input field
-          var selectedDates = $(this).dpGetSelected();
-          var result = new Array();
+    // create the datepicker and bind the "dateSelected" event
+    $('.datepicker')
+      .each(function () {
+        var inline = $(this).parents('form').has('.has-datepicker').length == 0;
 
-          for (var i in selectedDates) {
-            result.push(selectedDates[i].asString('yyyy-mm-dd'));
-          }
-
-          $('.date-for-datepicker', $(this).parents('form')).val(result.join(';'));
+        $(this).datePicker({
+          createButton: false,
+          displayClose: true,
+          inline: inline,
+          closeOnSelect: false,
+          selectMultiple: true
         });
+      })
+    	.bind('dateSelected', function (e, selectedDate, $td, state) {
+    	  // when picking a date, we add or remove it from the date input field
+        var selectedDates = $(this).dpGetSelected();
+        var result = new Array();
 
-      // highlighting selected dates in datepicker
-      $('.date-for-datepicker').each(function () {
-        if ($(this).val() != '') {
-          var dates = $(this).val().split(';');
-          for (var i in dates) {
-            var date = Date.fromString(dates[i], 'yyyy-mm-dd').asString('dd/mm/yyyy');
-            $('.datepicker', $(this).parents('form')).dpSetSelected(date);
-          }
+        for (var i in selectedDates) {
+          result.push(selectedDates[i].asString('yyyy-mm-dd'));
         }
+
+        $('.date-for-datepicker', $(this).parents('form')).val(result.join(';'));
       });
-    }
-  };
+
+    // highlighting selected dates in datepicker
+    $('.date-for-datepicker').each(function () {
+      if ($(this).val() != '') {
+        var dates = $(this).val().split(';');
+        for (var i in dates) {
+          var date = Date.fromString(dates[i], 'yyyy-mm-dd').asString('dd/mm/yyyy');
+          $('.datepicker', $(this).parents('form')).dpSetSelected(date);
+        }
+      }
+    });
+  }
+};
+
 })(jQuery);
