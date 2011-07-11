ffb189796ff857e85130d3b9681f2eec9409922e
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-11 19:22:04 +0200

    code cleanup

--- cnapi_ui/cnapi-ui-list.tpl.php
+++ cnapi_ui/cnapi-ui-list.tpl.php
@@ -2,11 +2,11 @@
   <p><?php print $no_results ?></p>
 <?php else : ?>
   <p><?php print $result_info ?></p>
-  
+
   <p><?php print t('Sort by') ?> : <?php print $sort_links ?></p>
-  
+
   <p><?php print $rss ?></p>
-  
+
   <ul>
   <?php foreach ($objects as $object) : ?>
     <li><?php print $object ?></li>
