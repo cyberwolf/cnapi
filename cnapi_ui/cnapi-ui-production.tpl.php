9f8ebb4117ecd9273f7adaca4f0c640c90b2d0fe
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-07-11 19:15:55 +0200

    adding directors, actors, other performers

--- cnapi_ui/cnapi-ui-production.tpl.php
+++ cnapi_ui/cnapi-ui-production.tpl.php
@@ -18,6 +18,33 @@
   <dd><?php print $longdescription ?></dd>
 <?php endif; ?>
 
+<?php if ($directors) : ?>
+  <dt>Directors</dt>
+  <dd>
+    <?php foreach ($directors as $director) : ?>
+      <?php print $director ?>,
+    <?php endforeach; ?>
+  </dd>
+<?php endif; ?>
+
+<?php if ($actors) : ?>
+  <dt>Actors</dt>
+  <dd>
+    <?php foreach ($actors as $actor) : ?>
+      <?php print $actor ?>,
+    <?php endforeach; ?>
+  </dd>
+<?php endif; ?>
+
+<?php if ($performers) : ?>
+  <dt>Other performers</dt>
+  <dd>
+    <?php foreach ($performers as $performer) : ?>
+      <?php print $performer ?>,
+    <?php endforeach; ?>
+  </dd>
+<?php endif; ?>
+
 <?php if ($images) : ?>
   <dt>Images</dt>
   <dd>
