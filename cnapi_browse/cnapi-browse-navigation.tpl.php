20e6f9b0a485bd1d86e5366c8ef8420efc34e2da
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-05 11:16:45 +0200

    adding extra classes to nav templates

--- cnapi_browse/cnapi-browse-navigation.tpl.php
+++ cnapi_browse/cnapi-browse-navigation.tpl.php
@@ -1,9 +1,9 @@
-<ul>
-<?php foreach ($items as $item) : ?>
-  <li>
+<ul class="navigation-level-<?php print $level++  ?>">
+<?php foreach ($items as $i => $item) : ?>
+  <li class="navigation-item-<?php print $i ?>">
     <?php print theme('cnapi_browse_navigation_item', array('item' => $item)) ?>
     <?php if (!empty($item['children'])) : ?>
-      <?php print theme('cnapi_browse_navigation', array('items' => $item['children'])); ?>
+      <?php print theme('cnapi_browse_navigation', array('items' => $item['children'], 'level' => $level)); ?>
     <?php endif; ?>
   </li>
 <?php endforeach; ?>
