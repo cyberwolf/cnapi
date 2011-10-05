692c7df0535a7a330f37e9100f10c3eaeac5d559
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-05 12:18:44 +0200

    casting  in nav template

--- cnapi_browse/cnapi-browse-navigation.tpl.php
+++ cnapi_browse/cnapi-browse-navigation.tpl.php
@@ -1,6 +1,6 @@
-<ul class="navigation-level-<?php print $level++  ?>">
+<ul class="navigation-level-<?php print (string)($level++)  ?>">
 <?php foreach ($items as $i => $item) : ?>
-  <li class="navigation-item-<?php print $i ?>">
+  <li class="navigation-item-<?php print (string)$i ?>">
     <?php print theme('cnapi_browse_navigation_item', array('item' => $item)) ?>
     <?php if (!empty($item['children'])) : ?>
       <?php print theme('cnapi_browse_navigation', array('items' => $item['children'], 'level' => $level)); ?>
