7818789f3fd8113845afa628dec55d98b2d0f290
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-10 15:05:12 +0200

    adding some spans and classes

--- cnapi_browse/cnapi-browse-filter-options.tpl.php
+++ cnapi_browse/cnapi-browse-filter-options.tpl.php
@@ -1,9 +1,9 @@
-<ul>
-<?php foreach ($options as $option) : ?>
-  <li>
+<ul class="filter-level-<?php print (string)($level++) ?>">
+<?php foreach ($options as $i => $option) : ?>
+  <li class="filter-option-<?php print (string)$i ?><?php print $option['active'] ? ' active' : '' ?>">
     <?php print theme('cnapi_browse_filter_option', array('option' => $option)); ?>
     <?php if (!empty($option['children'])) : ?>
-      <?php print theme('cnapi_browse_filter_options', array('options' => $option['children'])); ?>
+      <?php print theme('cnapi_browse_filter_options', array('options' => $option['children'], 'level' => $level)); ?>
     <?php endif; ?>
   </li>
 <?php endforeach; ?>
