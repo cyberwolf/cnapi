aed1b477461e14f15ea2e183baab43f4f778b895
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-17 16:47:22 +0200

    more work on cnapi-ui-production-summary

--- cnapi_ui/cnapi-ui-production-summary.tpl.php
+++ cnapi_ui/cnapi-ui-production-summary.tpl.php
@@ -1,32 +1,48 @@
-<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> teaser clearfix"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-  <h<?php print $heading_level ?><?php print $title_attributes; ?>>
-    <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
-  </h<?php print $heading_level ?>>
-  <?php print render($title_suffix); ?>
+  <div class="teaser-image">
 
-  <?php if ($thumbnail) : ?>
-    <?php print $thumbnail ?>
-  <?php endif; ?>
+	  <?php if ($thumbnail) : ?>
+	    <?php print $thumbnail ?>
+	  <?php endif; ?>
+  
+  </div>
 
-  <?php if ($shortdescription) : ?>
-    <div class="description">
-      <?php print $shortdescription ?>
-    </div>
-  <?php endif; ?>
+  <div class="teaser-text">
 
-  <?php if ($performers) : ?>
-    <dl>
+	  <?php print render($title_prefix); ?>
+	  <h<?php print $heading_level ?><?php print $title_attributes; ?>>
+	    <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
+	  </h<?php print $heading_level ?>>
+	  <?php print render($title_suffix); ?>
+	
+	  <?php if ($for_children) : ?>
+	    <?php print $vlieg_image ?>
+	  <?php endif; ?>
+	
+	  <?php if ($shortdescription) : ?>
+	    <div class="description">
+	      <?php print $shortdescription ?>
+	    </div>
+	  <?php endif; ?>
+	
+	  <?php if ($performers) : ?>
+	    <dl class="clearfix">
+	
+	      <?php if ($performers) : ?>
+	        <dt>Wie</dt>
+	        <dd><?php print $performers ?></dd>
+	      <?php endif; ?>
+	
+	    </dl>
+	  <?php endif; ?>
 
-      <?php if ($performers) : ?>
-        <dt>Wie</dt>
-        <dd><?php print $performers ?></dd>
-      <?php endif; ?>
-
-    </dl>
-  <?php endif; ?>
-
-  <?php print $more_link ?>
-
+      <div class="teaser-actions">
+	
+	    <?php print $more_link ?>
+	    
+	  </div>
+	  
+  </div>
+  
 </div>
