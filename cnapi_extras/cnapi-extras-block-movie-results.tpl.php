a904799c6885d8d4acdd7a33e75a2d1440c18d9a
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-25 14:34:22 +0200

    movie results

---
+++ cnapi_extras/cnapi-extras-block-movie-results.tpl.php
@@ -0,0 +1,5 @@
+<?php print $productions ?>
+<?php print $form ?>
+<?php if ($more_link) : ?>
+  <?php print $more_link ?>
+<?php endif; ?>
---
+++ cnapi_extras/cnapi-extras-block-movie.tpl.php
@@ -0,0 +1,3 @@
+<?php print $title ?>
+<?php print $thumbnail ?>
+<?php print $more_link ?>
