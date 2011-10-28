f293e6a9811d3a17b2be595a36fd349c34ffc768
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-10-28 12:25:55 +0200

    more work on tpl

--- cnapi_extras/cnapi-extras-block-movie-results.tpl.php
+++ cnapi_extras/cnapi-extras-block-movie-results.tpl.php
@@ -1,5 +1,10 @@
 <?php print $productions ?>
-<?php print $form ?>
+<div class="movieblock-form">
+  <h4>Volledig filmaanbod</h4>
+  <?php print $form ?>
+</div>
 <?php if ($more_link) : ?>
-  <?php print $more_link ?>
+  <div class="movieblock-more">
+    <?php print $more_link ?>
+  </div>
 <?php endif; ?>
--- cnapi_extras/cnapi-extras-block-movie.tpl.php
+++ cnapi_extras/cnapi-extras-block-movie.tpl.php
@@ -1,3 +1,7 @@
-<?php print $title ?>
-<?php print $thumbnail ?>
-<?php print $more_link ?>
+<div class="teaser-image">
+  <?php print $thumbnail ?>
+</div>
+<div class="teaser-text">
+  <h3><?php print $title ?></h3>
+  <span class="read-more"><?php print $more_link ?></span>
+</div>
