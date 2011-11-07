254048b0bac62343b40fbac6cf02ff299c34ef25
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2011-11-07 17:01:21 +0100

    summmary thumbs

--- cnapi_ui/cnapi-ui-event-mini-summary.tpl.php
+++ cnapi_ui/cnapi-ui-event-mini-summary.tpl.php
@@ -1,5 +1,13 @@
 <div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> mini-teaser clearfix"<?php print $attributes; ?>>
 
+  <div class="teaser-image">
+  
+    <?php if ($thumbnail) : ?>
+      <?php print $thumbnail ?>
+    <?php endif; ?>
+
+  </div>
+
   <?php print render($title_prefix); ?>
   <h<?php print $heading_level ?><?php print $title_attributes; ?>>
     <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
